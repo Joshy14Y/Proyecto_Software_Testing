06745b9f2c5479d20fe20284c54bf7a9e5ea374b
Joshua Sancho <joshygamer14Y@gmail.com>
2024-02-13 23:51:39 -0600

    F2

--- Software_Project/src/components/FilterComponent.jsx
+++ Software_Project/src/components/FilterComponent.jsx
@@ -1,9 +1,84 @@
-import { Accordion, Container, Col } from "react-bootstrap"
+import { Accordion, Container, Col, Row, Dropdown, ButtonGroup, Form, ToggleButton, InputGroup, Button } from "react-bootstrap"
+
+const radios = [
+  { name: 'Compra', value: '1' },
+  { name: 'Venta', value: '2' },
+  { name: 'Cualquiera', value: '3' },
+];
+
+const generateRadios = (radios, radioValue) => {
+  return radios.map((radio, idx) => (
+    <ToggleButton key={idx}
+      id={`radio-${idx}`}
+      type="radio"
+      variant="secondary"
+      name="radio"
+      value={radio.value}
+      checked={radioValue === radio.value}>
+      {radio.name}
+    </ToggleButton>
+  ));
+};
+
 
 const FilterComponent = () => {
-    return (
-   <></>
-    )
+  return (
+    <Row>
+      <Accordion>
+        <Accordion.Header>
+          Filtros
+        </Accordion.Header>
+        <Accordion.Body>
+          <Form>
+            <Row>
+              <Col md={4}>
+                <Row>
+                  <ButtonGroup>
+                    {generateRadios(radios)}
+                  </ButtonGroup>
+                </Row>
+                <Row>
+                  <InputGroup>
+                    <Button>-</Button>
+                    <Form.Control/>
+                    <Button>+</Button>
+                  </InputGroup>
+                </Row>
+              </Col>
+              <Col md={4}>
+                <Row>
+                  <ButtonGroup>
+                    {generateRadios(radios)}
+                  </ButtonGroup>
+                </Row>
+                <Row>
+                  <InputGroup>
+                    <Button>-</Button>
+                    <Form.Control/>
+                    <Button>+</Button>
+                  </InputGroup>
+                </Row>
+              </Col>
+              <Col md={4}>
+                <Row>
+                  <ButtonGroup>
+                    {generateRadios(radios)}
+                  </ButtonGroup>
+                </Row>
+                <Row>
+                  <InputGroup>
+                    <Button>-</Button>
+                    <Form.Control/>
+                    <Button>+</Button>
+                  </InputGroup>
+                </Row>
+              </Col>
+            </Row>
+          </Form>
+        </Accordion.Body>
+      </Accordion>
+    </Row>
+  )
 }
 
 export default FilterComponent
